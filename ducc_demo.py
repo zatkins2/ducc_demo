9539e6384b8d9148201e3e1cae939960b48d53af
Zachary Atkins <zatkins2@gmail.com>
2026-08-20 10:52:18 -0400

    now synthesis as well

--- ducc_demo.py
+++ ducc_demo.py
@@ -43,7 +43,8 @@ ducc_geometry = curvedsky.get_ducc_geo(shape=imap.shape, wcs=imap.wcs)
 print(f'Map shape: {shape}')
 print(f'Map dtype: {imap.dtype}')
 print(f'Map wcs: {wcs}')
-print(f'Map quadrature scheme: {ducc_geometry.name}') # should be 'F1'
+assert ducc_geometry.name == 'F1', f'Expected F1, got {ducc_geometry.name=}'
+print(f'Map quadrature scheme: {ducc_geometry.name}')
 
 # first we will compute the spherical harmonic coefficients (the "alm" values)
 # via a spherical harmonic analysis (map --> alm). we will do so inside some
@@ -86,15 +87,15 @@ flipped_masked_imap = masked_imap[..., ::-1, ::-1]
 #    scheme which is 'F1' in this case. The pixelization must follow one of the 
 #    quadrature schemes
 
-# in either case we also need to multiply by the quadrature weights. the weights
+###############################################################################
+############################### Do 1st method #################################
+
+# in this case we also need to multiply by the quadrature weights. the weights
 # are for the full dec range, so we need to slice-out the exact rows for our map
 fullsky_quadw = sht.get_gridweights(ducc_geometry.name, ducc_geometry.ny)
 fullsky_quadw /= ducc_geometry.nx
 fullsky_quadw = fullsky_quadw.astype(imap.dtype, copy=False)
 quadw = fullsky_quadw[ducc_geometry.yoff:ducc_geometry.yoff + imap.shape[-2]]
-
-###############################################################################
-############################### Do 1st method #################################
 
 # get necessary arguments for ducc. these will be the co-latitudes of 
 # each ring as an array in 'theta', the number of columns in each ring
@@ -105,7 +106,7 @@ ducc_rings = curvedsky.get_ring_info(*flipped_masked_imap.geometry)
 # perform transform (spin-0 and spin-2). here is where you would try a new
 # implementation and compare results!
 alm_T = sht.adjoint_synthesis(
-    map=(quadw[..., None] * flipped_masked_imap)[0].reshape(-1)[None], spin=0,
+    map=(quadw[..., None] * flipped_masked_imap)[0:1].reshape(1, -1), spin=0,
     lmax=lmax, theta=ducc_rings.theta, nphi=ducc_rings.nphi,
     phi0=ducc_rings.phi0, ringstart=ducc_rings.offsets,
     nthreads=nthreads
@@ -128,15 +129,21 @@ np.save(opj(data_dir, 'ducc_alm_TEB_adjoint_synthesis.npy'), alm_TEB)
 ###############################################################################
 ############################### Do 2nd method #################################
 
+# NOTE: no quadrature weights manually applied, ducc supplies them internally
+
 # need to insert the data into a full-sky array first.
 # NOTE: here, i am manually inserting what i know to be the correct res and variant.
 # this just gives me an array with the correct shape and wcs for a fullsky map
-fullsky_masked_imap_shape, fullsky_masked_imap_shape_wcs = enmap.fullsky_geometry(res=np.deg2rad(1/120), variant='fejer1')
+fullsky_masked_imap_shape, fullsky_masked_imap_shape_wcs = enmap.fullsky_geometry(
+    res=np.deg2rad(1/120), variant='fejer1'
+    )
+
 flipped_fullsky_masked_imap = enmap.zeros(
     shape=(3, *fullsky_masked_imap_shape),
     wcs=fullsky_masked_imap_shape_wcs,
     dtype=masked_imap.dtype
     )[..., ::-1, ::-1]
+
 flipped_fullsky_masked_imap[..., ducc_geometry.yoff:ducc_geometry.yoff + imap.shape[-2], :] = flipped_masked_imap
 
 # get necessary arguments for ducc. this is just the single value giving
@@ -163,7 +170,117 @@ alm_EB = sht.analysis_2d(
 
 alm_TEB = np.concatenate([alm_T, alm_EB], axis=0)
 
-np.save(opj(data_dir, 'ducc_alm_TEB_adjoint_synthesis_2d.npy'), alm_TEB)
+np.save(opj(data_dir, 'ducc_alm_TEB_analysis_2d.npy'), alm_TEB)
 
 ###############################################################################
 ###############################################################################
+
+# now do synthesis operation (alm --> map). because the analysis operation is
+# bandlimited, the synthesized map will not be equivalent to the analyzed map;
+# instead it will scales with multipole higher than the bandlimit (i.e., 
+# with physically smaller wavelengths). as before, we can do this in two ways:
+# 1. We can use sht.synthesis. We need to give ducc more info about the
+#    declination rings; in principle these don't have to follow one of the above
+#    geometries with exact quadrature weights (but in this case, they do)
+# 2. We can use sht.synthesis_2d if we "extract" the cut map out of a
+#    a map that spans the full declination range. We only specify the quadrature
+#    scheme which is 'F1' in this case. The pixelization must follow one of the 
+#    quadrature schemes
+
+###############################################################################
+############################### Do 1st method #################################
+
+# NOTE: quadrature weights only needed for analysis, not synthesis!
+
+# get necessary arguments for ducc. these will be the co-latitudes of 
+# each ring as an array in 'theta', the number of columns in each ring
+# in 'nphi', the starting longitude of each ring in 'phi0', and the
+# index into the flattened 2d map where each ring starts in 'ringstart'
+ducc_rings = curvedsky.get_ring_info(*flipped_masked_imap.geometry)
+
+# perform transform (spin-0 and spin-2). here is where you would try a new
+# implementation and compare results!
+map_I = sht.synthesis(
+    alm=alm_T, spin=0,
+    lmax=lmax, theta=ducc_rings.theta, nphi=ducc_rings.nphi,
+    phi0=ducc_rings.phi0, ringstart=ducc_rings.offsets,
+    nthreads=nthreads
+    )
+
+map_QU = sht.synthesis(
+    alm=alm_EB, spin=2, 
+    lmax=lmax, theta=ducc_rings.theta, nphi=ducc_rings.nphi,
+    phi0=ducc_rings.phi0, ringstart=ducc_rings.offsets,
+    nthreads=nthreads
+    )
+
+map_IQU = np.concatenate([map_I, map_QU], axis=0)
+
+# now we need to reshape the map because its last axis is 1d, and 
+# flip it back to the original declination and right-ascension ordering.
+# finally, reassign original wcs information and write to disk.
+map_IQU = map_IQU.reshape(*flipped_masked_imap.shape)
+map_IQU = map_IQU[..., ::-1, ::-1]
+map_IQU = enmap.ndmap(map_IQU, masked_imap.wcs)
+
+enmap.write_map(opj(data_dir, 'ducc_map_IQU_synthesis.fits'), map_IQU)
+
+###############################################################################
+###############################################################################
+
+# plot to visualize the synethized map and compare to the original map
+for i in range(3):
+    p = enplot.plot(map_IQU[i], downgrade=32, colorbar=True, ticks=15)
+    p_diff = enplot.plot(map_IQU[i] - masked_imap[i], downgrade=32, colorbar=True, ticks=15)
+
+    enplot.write(opj(data_dir, f"synth_adj_synth_act_dr6_planck_coadd_{'IQU'[i]}"), p)
+    enplot.write(opj(data_dir, f"synth_adj_synth_vs_original_act_dr6_planck_coadd_{'IQU'[i]}"), p_diff)
+
+###############################################################################
+############################### Do 2nd method #################################
+
+# perform transform (spin-0 and spin-2). here is where you would try a new
+# implementation and compare results!
+map_I = sht.synthesis_2d(
+    alm=alm_T, spin=0,
+    lmax=lmax, geometry=ducc_geometry.name,
+    phi0=ducc_info.phi0, ntheta=ducc_geometry.ny, nphi=ducc_geometry.nx,
+    nthreads=nthreads
+    )
+
+map_QU = sht.synthesis_2d(
+    alm=alm_EB, spin=2,
+    lmax=lmax, geometry=ducc_geometry.name,
+    phi0=ducc_info.phi0, ntheta=ducc_geometry.ny, nphi=ducc_geometry.nx,
+    nthreads=nthreads
+    )
+
+map_IQU = np.concatenate([map_I, map_QU], axis=0)
+
+# need to extract the data out of the full-sky array now, and 
+# flip it back to the original declination and right-ascension ordering.
+# finally, reassign original wcs information and write to disk.
+map_IQU = map_IQU[..., ducc_geometry.yoff:ducc_geometry.yoff + imap.shape[-2], :]
+map_IQU = map_IQU[..., ::-1, ::-1]
+map_IQU = enmap.ndmap(map_IQU, masked_imap.wcs)
+
+enmap.write_map(opj(data_dir, 'ducc_map_IQU_synthesis_2d.fits'), map_IQU)
+
+###############################################################################
+###############################################################################
+
+# plot to visualize the synethized map and compare to the original map
+for i in range(3):
+    p = enplot.plot(map_IQU[i], downgrade=32, colorbar=True, ticks=15)
+    p_diff = enplot.plot(map_IQU[i] - masked_imap[i], downgrade=32, colorbar=True, ticks=15)
+
+    enplot.write(opj(data_dir, f"synth2d_ana2d_act_dr6_planck_coadd_{'IQU'[i]}"), p)
+    enplot.write(opj(data_dir, f"synth2d_ana2d_vs_original_act_dr6_planck_coadd_{'IQU'[i]}"), p_diff)
+
+# plot to visualize the synethized map and compare to the original map
+for i in range(3):
+    p = enplot.plot(map_IQU[i], downgrade=32, colorbar=True, ticks=15)
+    p_diff = enplot.plot(map_IQU[i] - masked_imap[i], downgrade=32, colorbar=True, ticks=15)
+
+    enplot.write(opj(data_dir, f"synth2d_ana2d_act_dr6_planck_coadd_{'IQU'[i]}"), p)
+    enplot.write(opj(data_dir, f"synth2d_ana2d_vs_original_act_dr6_planck_coadd_{'IQU'[i]}"), p_diff)
